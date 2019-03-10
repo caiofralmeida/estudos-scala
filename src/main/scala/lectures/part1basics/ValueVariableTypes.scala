075b14947826038655a2f3cb50e06c0899b09bdd
Caio Almeida <caio.f.r.amd@gmail.com>
2019-03-10 02:26:46 +0000

    Add value variable types of scala

--- src/main/scala/lectures/part1basics/ValueVariableTypes.scala
+++ src/main/scala/lectures/part1basics/ValueVariableTypes.scala
@@ -1,5 +1,19 @@
 package lectures.part1basics
 
 object ValueVariableTypes extends App {
+  /**
+    * immutables = val
+    */
+  val int: Int = 10
+  val str: String = "Foo Bar"
+  val double: Double = 0.2
+  val float: Float = 2f
+  val long: Long = 1L
+  val bigInt: BigInt = 1000000000
 
+  /**
+    * mutables = var (side effects)
+    */
+  var x = "test"
+  x = "foo"
 }
